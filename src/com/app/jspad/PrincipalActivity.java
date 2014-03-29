edf12ea7c04f22bf52775a6e9987b4a4c56a1089
jonnymohamed <jonny.suptitz@gmail.com>
2014-03-28 21:01:22 -0300

    testado estado de conexao com a internet

--- src/com/app/jspad/PrincipalActivity.java
+++ src/com/app/jspad/PrincipalActivity.java
@@ -1,6 +1,7 @@
 package com.app.jspad;
 
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -18,21 +19,19 @@ public class PrincipalActivity extends Activity {
         
         setContentView(R.layout.principal);//carrega tela
         
+        final boolean conectado = isOnline(this);
+        
         //botao para joystick
         Button btJoystick = (Button)findViewById(R.id.btJoystick);
         //abre tela conectar
         btJoystick.setOnClickListener(new OnClickListener() {
         	public void onClick ( View arg0 ) {
-        		Context ctx = null;
-        		
-        		ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
-        		
         		//verifica se wifi esta conectado
-        		if (cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected()){
+        		if (conectado){
         			Intent intent = new Intent(PrincipalActivity.this, ConectarActivity.class);
         			startActivity(intent);
         		}else{
-        			Toast.makeText(ctx, "Sem conexão com a internet.", Toast.LENGTH_SHORT).show();
+        			Toast.makeText(PrincipalActivity.this, "Sem conexão com a internet.", Toast.LENGTH_SHORT).show();
         		}
         }
     });
@@ -56,6 +55,21 @@ public class PrincipalActivity extends Activity {
         }
     });
       
- };    
+ }; 
+ 
+ public static boolean isOnline(Context contexto){
+	 //Pego a conectividade do contexto o qual o metodo foi chamado
+	 ConnectivityManager cm = (ConnectivityManager) contexto.getSystemService(Context.CONNECTIVITY_SERVICE);
+	 //Crio o objeto netInfo que recebe as informacoes da NEtwork
+	 NetworkInfo netInfo = cm.getActiveNetworkInfo();
+	 //Se o objeto for nulo ou nao tem conectividade retorna false
+	 
+	 if ( (netInfo != null) && (netInfo.isConnectedOrConnecting()) && (netInfo.isAvailable()) ){
+		 return true;
+	 }
+	 else{
+	 	return false;
+	 }
+ }
      
 }
